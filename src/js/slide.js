597e01c6a7b8ef8ae9385ae32a3156db907f32a6
sungeun93 <tjddmsndbfl@naver.com>
2022-12-08 14:57:51 +0900

    슬라이드

--- src/js/slide.js
+++ src/js/slide.js
@@ -52,7 +52,7 @@ function mainSlide() {
         slidePage.innerText = `${index+1}/${slideContainer.childElementCount}`
     }
     
-    }
+}
 /* 메인 슬라이드 */
 
 
@@ -60,111 +60,72 @@ function mainSlide() {
 
 function subSlide() {
 
-    const tabSlide = document.getElementsByClassName('tab-menu-container');
+    const tabSlide = document.querySelector('.tab-menu-container');
     const innerSlider = document.querySelectorAll('#tab1');
     const slideTab = document.querySelectorAll('#tab2');
-    let savePosition = 0
-    let pressed = false // 클릭 상태 체크
-    let startx // 마우스 드래그 시작점 x좌표
-    let x // 마우스 드래그시 x좌표
+    let index = 0;
+    let slideWidth = 400;
+    let slideIterval;
+    let pressed = false;
+    let startPoint = 0;
+    let endPoint = 0;
+
+    slideIterval = setInterval(()=>{
+        next();    
+        },5000)
+
+    function next() {
+        index++;
+
+        if(index===tabSlide.childElementCount) {
+            index=0
+        }
+        move();
+    }
+
+    function prev() {
+        index--;
+
+        if(index===-1) {
+            index = tabSlide.childElementCount-1;
+        }
+        move();
+    }
+
+    function move() {
+        tabSlide.setAttribute('style', `transform:translateX(-${400*index}px)`)
+        console.log('moved!')
+        clearInterval(slideIterval);
+        slideIterval = setInterval(()=>{
+        next();    
+        },5000)
+    }
     
+    tabSlide.addEventListener("mousedown", (e) => {
+        pressed = true;
+        startPoint = e.pageX;
+        clearInterval(slideIterval);
+    });
     
-    
-    
-    window.addEventListener("mouseup", () => {
-        pressed = false
+    tabSlide.addEventListener("mouseup", (e) => {
+        pressed = false;
+        endPoint = e.pageX;
+        slideIterval = setInterval(()=>{
+            next();    
+        },5000)
+        
+        if(startPoint < endPoint) {
+            prev();
+        }else if(startPoint > endPoint) {
+            next();
+        }
     })
     
-    for(let i = 0 ; i < tabSlide.length ; i ++){
-    
-        tabSlide[i].addEventListener("mousedown", e => {
-            pressed = true
-            startx = checkPosX(e)
-    
-            tabSlide[i].style.cursor = "grabbing";
-            innerSlider[i].style.transition = `0.5s`;
-        })
-        
-        tabSlide[i].addEventListener("mouseup", () => {
-            tabSlide[i].style.cursor = "grab";
-            savePosition = startx + x;
-            innerSlider[i].style.transition = `0.5s`;
-            x=0;
-        })
-    
-        tabSlide[i].addEventListener("mousemove", e => {
-            if (!pressed) return
-            e.preventDefault()
-            x = checkPosX(e) - startx;
-            console.log(startx, x)
-            innerSlider[i].style.transform = `translateX(-${savePosition - x}px)`;
-            checkboundary(tabSlide[i],innerSlider[i])
-        })
-    }
-    
-    function checkboundary(tas,ins) {
-        let outer = tas.getBoundingClientRect()
-        let inner = ins.getBoundingClientRect()
-    
-        if (parseInt(ins.style.left) > 0) {
-            ins.style.transform = "0px"
-        } else if (inner.right < outer.right) {
-            ins.style.left = `-${inner.width - outer.width}px`
-        }
-    }
-
-    function checkPosX(e) {
-    
-        return e.clientX;
-    }
-/* 탭1 */
-
-
-    window.addEventListener("mouseup", () => {
-        pressed = false
+    tabSlide.addEventListener("mousemove",(e) => {
+        if(!pressed) return;
+        e.preventDefault();
+        const x = e.pageX - startPoint;
+        innerSlider[0].style.transform = `translateX(-${index*(slideWidth + 150)-x}px)`
     })
-    
-    for(let i = 0 ; i < tabSlide.length ; i ++){
-    
-        tabSlide[i].addEventListener("mousedown", e => {
-            pressed = true
-            startx = checkPosX(e)
-    
-            tabSlide[i].style.cursor = "grab";
-            slideTab[i].style.transition = `0.5s`;
-        })
-        
-        tabSlide[i].addEventListener("mouseup", () => {
-            tabSlide[i].style.cursor = "grab";
-            savePosition = startx + x;
-            slideTab[i].style.transition = `0.5s`;
-            x=0;
-        })
-    
-        tabSlide[i].addEventListener("mousemove", e => {
-            if (!pressed) return
-            e.preventDefault()
-            x = checkPosX(e) - startx;
-            console.log(startx, x)
-            slideTab[i].style.transform = `translateX(-${savePosition - x}px)`;
-            checkboundary(tabSlide[i],slideTab[i])
-        })
-    }
-    
-    function checkboundary(tas,ins) {
-        let outer = tas.getBoundingClientRect()
-        let inner = ins.getBoundingClientRect()
-    
-        if (parseInt(ins.style.left) > 0) {
-            ins.style.left = "0px"
-        } else if (inner.right < outer.right) {
-            ins.style.left = `-${inner.width - outer.width}px`
-        }
-    }
-    
-    function checkPosX(e) {
-    
-        return e.clientX;
-    }
-/* 탭2 */
+
 }
